4f38a2ce2ad042e7ff2b817820fb55b0ee011013
dasifue <dasifue@gmail.com>
2024-07-16 19:53:46 +0600

    swagger

--- main/settings.py
+++ main/settings.py
@@ -44,6 +44,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'drf_yasg',
+    'rest_framework_swagger',
     'rest_framework_simplejwt',
     'rest_framework',
     'shop',
